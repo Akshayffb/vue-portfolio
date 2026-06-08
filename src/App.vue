3c21ece0601843875a8f2a8d65a760fabe5f666f
akshayffb <akshayffb@gmail.com>
2026-06-08 23:14:56 +0530

    small fix

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,12 @@
 import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-const show = ref(false);
-const counter = ref(0);
+import { RouterView } from 'vue-router';
 
-const increase = () => {
-  counter.value++;
-  // console.log('clicked');
-};
-
-const decrease = () => {
-  if(counter.value > 0){
-    counter.value--;
-    return;
-  }
-  window.alert("Counter reached 0, can't be made negative");
-}
-
-const reset = () => {
-counter.value = 0;
-}
 </script>
 
 <template>
-  <header v-if="show">
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
-    <!-- <div class="wrapper"> -->
-    <!-- </div> -->
-  </header>
-
-  <h1>{{ counter }}</h1>
-  <button @click="increase" class="btn btn-primary">Increase</button>
-    <button @click="decrease" class="btn">Decrease</button>
-    <button @click="reset">Reset</button>
-  <main>
-    <HelloWorld msg="Hi, this is Akshay ffb" />
-    <slot></slot>
-  </main>
+  <Routerview />
 </template>
 
 <style scoped>
@@ -45,10 +15,10 @@ header {
   line-height: 1.5;
 }
 
-.btn{
+.btn {
   border: 1px solid #B9B9B9;
   padding: 15px;
-  background-color:black;
+  background-color: black;
   color: white;
   border-radius: 20px;
 }
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+createApp(App).use().mount('#app')
