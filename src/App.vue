eb8a1516eeab53427380f5e97523c95ec30c457a
akshayffb <akshayffb@gmail.com>
2026-06-08 23:42:30 +0530

    updated router, work in progress page

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,10 @@
 <script setup>
-import { ref } from 'vue';
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import { RouterView } from 'vue-router';
+import WorkInProgress from './components/WorkInProgress.vue';
 
+const isLIve = false;
 </script>
 
 <template>
-  <Routerview />
+  <WorkInProgress v-if="!isLIve" />
+  <router-view v-else />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.btn {
-  border: 1px solid #B9B9B9;
-  padding: 15px;
-  background-color: black;
-  color: white;
-  border-radius: 20px;
-}
-
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import router from "./router/index"
 
-createApp(App).use().mount('#app')
+createApp(App).use(router).mount('#app')
